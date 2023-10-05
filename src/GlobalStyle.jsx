1fc1fe8134b23b23270cc05eb3aa6281af2deeaa
Ethen1264 <jxorhks86909763@gmail.com>
2023-10-05 21:06:36 +0900

    add header and Globalstyle

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,13 @@
+// 
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body, ul, li {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+} 
+`;
+
+export default GlobalStyle;
